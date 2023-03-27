30655d5e41d0a19963f3b1fd84bc172455b20f6b
raymondlouie <raylouie@hotmail.com>
2023-03-27 21:56:54 +1100

    Update wrapperPlots.R

--- R/wrapperPlots.R
+++ R/wrapperPlots.R
@@ -1,7 +1,6 @@
 #' Plot markers identified by each method on a heatmap
 #'
 #' @param list_markers List of selected markers from each method, from output of findClusterMarkers function.
-#' @param tile_text_size Tile text size
 #' @param text_size Text size
 #' @param color_tile Color of each tile
 #' @param fill_tile Fill of each tile
@@ -10,24 +9,23 @@
 #' @return A ggplot output
 #' @export
 plotMarkers =function(list_markers,
-                      tile_text_size=7,
                       text_size=40,
                       color_tile = "blue",
                       fill_tile = "blue",
                       coord_ratio  = 0.2,
                       ...){
-
-
+    
+    
     markers_df = reshape2::melt(data.frame(list_markers),
                                 measure.vars = names(list_markers))
     table_markers_df = data.frame(table(markers_df$value))
     table_markers_df = table_markers_df[order(table_markers_df$Freq,decreasing=TRUE),]
-
+    
     # Order markers according to most frequent.
     markers_df$value = factor(markers_df$value,
                               levels=rev(table_markers_df$Var1))
     markers_df = markers_df[!is.na(markers_df$value),]
-
+    
     p1=ggplot(markers_df, aes(x = variable, y = value),...) +
         geom_tile(color=color_tile,fill=fill_tile) + theme_classic()+
         theme(axis.text = element_text(size=text_size),
@@ -42,21 +40,23 @@ plotMarkers =function(list_markers,
 #' Plot performance of each marker
 #'
 #' @param list_performance List of performance metrics, from output of performanceAllMarkers function.
+#' @param tile_text_size Tile text size
 #' @param text_size Text size
 #'
 #' @return A list of ggplot outputs
 #' @export
 plotPerformance =function(list_performance,
+                          tile_text_size=7,
                           text_size=40,
                           ...){
-
+    
     # Create dataframe suitable for plotting
     performance_df = data.frame(list_performance)
     
     for (i in seq(1,dim(performance_df)[2],2)){
         curr_df = performance_df[,c(i,i+1)]
         tempSplit = unlist(lapply(as.character(colnames(curr_df)),
-                           function (x) strsplit(x,split="[.]")[[1]]))
+                                  function (x) strsplit(x,split="[.]")[[1]]))
         colnames(curr_df) = c("Clusters","TP")
         curr_df$marker_method=tempSplit[[1]]
         curr_df$performance_method=gsub("_performance",
@@ -87,19 +87,19 @@ plotPerformance =function(list_performance,
     #                                                        function (x) x[[2]]))
     # performance_plot_df$performance_method = gsub("_performance","",performance_plot_df$performance_method)
     performance_plot_df$TPround = round(performance_plot_df$TP,digits=3)
-
-
+    
+    
     unique_performance = unique(performance_plot_df$performance_method)
-
+    
     performance_plot_df$marker_method = factor(performance_plot_df$marker_method,
                                                levels = names(list_performance))
-
+    
     list_p1 = list()
     for (i in 1:length(unique_performance)){
-
+        
         curr_performance = unique_performance[[i]]
         curr_plot = performance_plot_df[which(performance_plot_df$performance_method %in% curr_performance),]
-
+        
         p1=ggplot(curr_plot, aes(x = marker_method, y = Clusters,fill=TP)) +
             geom_tile(color="black") + theme_bw()+
             scale_fill_gradient(low = "white", high = "red") +
@@ -116,7 +116,7 @@ plotPerformance =function(list_performance,
         list_p1[[i]] = p1
         print(p1)
     }
-
+    
     return(list_p1)
-
+    
 }
