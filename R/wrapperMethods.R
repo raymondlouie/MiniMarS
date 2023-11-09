4ded8eb6dc1b21a36432d3d949fedcef5184bec4
Raymond Louie <raylouie@hotmail.com>
2023-11-09 14:41:22 +1100

    Update wrapperMethods.R

--- R/wrapperMethods.R
+++ R/wrapperMethods.R
@@ -111,8 +111,8 @@ sc2markerWrapper <- function (input_matrix,
     seurat_object = Seurat::CreateSeuratObject(input_matrix,
                                                meta.data =data.frame(cell_type=clusters) )
     Seurat::Idents(object = seurat_object)=clusters
-    seruat_object@assays$RNA@counts = input_matrix
-    seruat_object@assays$RNA@data = input_matrix
+    seurat_object@assays$RNA@counts = input_matrix
+    seurat_object@assays$RNA@data = input_matrix
     
     message("here2")
     
