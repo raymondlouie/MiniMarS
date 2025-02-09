6d48fe2cf2392b7393388b70c4200a40dd5741c1
raymondlouie <raylouie@hotmail.com>
2025-02-09 18:45:10 +1100

    Update wrapperMethods.R

--- R/wrapperMethods.R
+++ R/wrapperMethods.R
@@ -563,7 +563,8 @@ calculateConsensus_wrap <- function(list_markers_temp,
         message("Only one method chosen.")
         names(list_markers_temp) = paste0(names(list_markers_temp),"_","consensus")
         list_markers = list_markers_temp
-        runtime_secs = 0
+        runtime_secs = c("consensus top" = 0)
+
     }
     
     fstat=apply(t(input_matrix),2,function (x) na.omit(anova(aov(x~as.factor(clusters)))$"F value"))
